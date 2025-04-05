561bfabbee5fd1181df871f24cbb206100ca8591
BagdalF <bagdalfelipe@gmail.com>
2025-04-04 21:35:40 -0300

    Refinamento da página de detalhes

--- data/items.php
+++ data/items.php
@@ -4,25 +4,29 @@ $items = [
         'id' => 1,
         'title' => 'Curso de PHP',
         'category' => 'Programação',
-        'image' => 'images/php-course.jpg'
+        'image' => 'images/php-course.jpg',
+        'description' => 'Aprenda PHP do básico ao avançado, incluindo conceitos de programação orientada a objetos.'
     ],
     [
         'id' => 2,
         'title' => 'Curso de JavaScript',
         'category' => 'Programação',
-        'image' => 'images/js-course.jpg'
+        'image' => 'images/js-course.jpg',
+        'description' => 'Domine JavaScript e desenvolva aplicações web dinâmicas e interativas.'
     ],
     [
         'id' => 3,
         'title' => 'Curso de Design Gráfico',
         'category' => 'Design',
-        'image' => 'images/design-course.jpg'
+        'image' => 'images/design-course.jpg',
+        'description' => 'Explore os fundamentos do design gráfico e crie projetos visuais incríveis.'
     ],
     [
         'id' => 4,
         'title' => 'Curso de Photoshop',
         'category' => 'Design',
-        'image' => 'images/ps-course.jpg'
+        'image' => 'images/ps-course.jpg',
+        'description' => 'Aprenda a usar o Photoshop para edição de imagens e design profissional.'
     ],
 ];
 ?>
--- detalhes.php
+++ detalhes.php
@@ -31,6 +31,7 @@ if (!$item) {
         <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>">
         <h2><?php echo $item['title']; ?></h2>
         <p>Categoria: <?php echo $item['category']; ?></p>
+        <p>Descrição: <?php echo $item['description']; ?></p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nisi id libero tincidunt tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
         <a href="index.php" class="btn">Voltar</a>
     </div>
