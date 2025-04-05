94f9a629f4355fe56d284ecb7729478f5325b4e5
BagdalF <bagdalfelipe@gmail.com>
2025-04-04 21:49:52 -0300

    Melhor estilização

--- details.php
+++ details.php
@@ -26,14 +26,14 @@ if (!$item) {
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Detalhes do Curso</h1>
+    <a href="index.php" class="btn">Voltar</a>
+    <h1>Detalhes do Curso </h1>
     <div class="details">
         <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>">
         <h2><?php echo $item['title']; ?></h2>
         <p>Categoria: <?php echo $item['category']; ?></p>
         <p>Descrição: <?php echo $item['description']; ?></p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nisi id libero tincidunt tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
-        <a href="index.php" class="btn">Voltar</a>
     </div>
 </body>
 </html>
